aea54db7403e33ee52facbb56d17a86c1dff438a
TanKittipat <kittipat12tan@gmail.com>
2024-06-17 15:18:32 +0700

    Create Card.

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,27 @@ function App() {
         </header>
       </div>
       <div className="search" id="search">
-        <input type="text" className="form-control" id="search-bar" placeholder="Search"/>
+        <input
+          type="text"
+          className="form-control"
+          id="search-bar"
+          placeholder="Search"
+        />
+      </div>
+      <div className="card-lists" id="card-lists">
+        <div className="card">
+          <img
+            src="https://food-cms.grab.com/compressed_webp/items/THITE2021042006463787451/detail/menueditor_item_a9c94c76fca2412eaf15471ec714c62b_1658287441624692726.webp"
+            alt=""
+          />
+          <div class="card-body">
+            <h6 class="card-title">กาแฟบุตรี - เสาชิงช้า</h6>
+            <p class="card-text">ชา กาแฟ</p>
+            <a href="#" class="btn btn-primary">
+              Order now!
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
